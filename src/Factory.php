73ea4458873516444bd51286b2bcec7c454537a5
蔡嘉俊 <caijiajun@caijiajundeMacBook-Pro.local>
2020-07-31 17:01:36 +0800

    修改变量名

--- src/Factory.php
+++ src/Factory.php
@@ -11,17 +11,17 @@ use CityService\Exceptions\CityServiceException;
 class Factory
 {
     /**
-     * @param string $platform
+     * @param string $driver
      * @param array  $config
      *
      * @return CityServiceInterface
      * @throws CityServiceException
      */
-    public static function getInstance($platform, array $config = []): CityServiceInterface
+    public static function getInstance($driver, array $config = []): CityServiceInterface
     {
         try {
-            $platform = ucwords($platform);
-            $class = new \ReflectionClass(__NAMESPACE__. "\\Drivers\\{$platform}\\{$platform}");
+            $driver = ucwords($driver);
+            $class = new \ReflectionClass(__NAMESPACE__. "\\Drivers\\{$driver}\\{$driver}");
             return $class->newInstanceArgs([$config]);
         }
         catch (\ReflectionException $e)
